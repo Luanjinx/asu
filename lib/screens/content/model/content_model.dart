1d2726db83fadb8219fc4a135c3fea013106469f
Luanjinx <sahruldarmian97@gmail.com>
2026-09-19 15:44:28 +0700

    Add files via upload

--- lib/screens/content/model/content_model.dart
+++ lib/screens/content/model/content_model.dart
@@ -201,8 +201,6 @@ class ContentData {
   String category;
   TvShowData? tvShowData;
   List<SeasonData> seasonList;
-  int? seasonId;
-  int? episodeNumber;
 
   String get releaseYear => releaseDate.isNotEmpty ? DateTime.tryParse(releaseDate)?.year.toString() ?? "" : "";
 
@@ -250,8 +248,6 @@ class ContentData {
     this.requiredPlanLevel = -1,
     this.tvShowData,
     this.seasonList = const <SeasonData>[],
-    this.seasonId,
-    this.episodeNumber,
   });
 
   factory ContentData.fromDetailsJson(Map<String, dynamic> json) {
@@ -289,8 +285,6 @@ class ContentData {
                   SeasonData.fromJson(json['season_data']),
                 ]
               : [],
-      seasonId: json['season_id'] is int ? json['season_id'] : (json['season_id'] is String ? int.tryParse(json['season_id']) : null),
-      episodeNumber: json['episode_number'] is int ? json['episode_number'] : (json['episode_number'] is String ? int.tryParse(json['episode_number']) : null),
     );
   }
 
@@ -319,8 +313,6 @@ class ContentData {
       'required_plan_level': requiredPlanLevel,
       'tv_show_data': tvShowData?.toJson(),
       'season_data': seasonList.map((e) => e.toJson()).toList(),
-      'season_id': seasonId,
-      'episode_number': episodeNumber,
     };
   }
 
